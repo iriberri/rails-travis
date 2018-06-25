8fab89e49ab864de7ace6f797fc19a032d3756e2
Joffrey <joffrey.berrier@gmail.com>
2018-06-26 00:49:42 +0200

    add new test

--- cypress/integration/views/pages/home_spec.js
+++ cypress/integration/views/pages/home_spec.js
@@ -16,6 +16,28 @@ context('Window', () => {
     cy.get('.btn').click();
   })
 
+  describe('Testing URL', () => {
+    it('can access to url', () => {
+      cy.visit('http://127.0.0.1:3000/users/sign_in');
+    });
+  });
+
+  describe('Testing Connection', () => {
+    it('can access to connect', () => {
+      cy.get('[href="/users/sign_in"]').click();
+      cy.get('#user_email').then(($input) => {
+        $input.val('user1@gmail.com')
+      });
+      cy.get('#user_password').then(($input) => {
+        $input.val('testtest')
+      });
+      cy.get('#new_user').submit()
+      if (cy.get('.alert.alert-warning')) {
+        return 'Invalid Email or password'
+      }
+    });
+  });
+
   describe('Testing Home', () => {
     it('can i have Hello Worl add numbers', () => {
       cy.get('#app').contains('Hello World')
