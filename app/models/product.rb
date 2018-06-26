b3db7251148eb12713f550483bf6a8d267f8718a
Joffrey <joffrey.berrier@gmail.com>
2018-06-26 09:28:25 +0200

    remove verification

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :user
   
-  validates :name, presence: true, uniqueness: true
-  validates :url, presence: true, uniqueness: true
-  validates :category, inclusion: { in: %w(tech education design), message: "%{value} is not a valid category" }
+  # validates :name, presence: true, uniqueness: true
+  # validates :url, presence: true, uniqueness: true
+  # validates :category, inclusion: { in: %w(tech education design), message: "%{value} is not a valid category" }
 end
