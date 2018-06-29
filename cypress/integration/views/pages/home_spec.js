fb2a4dbf1cb8870b919ef626e27d8b5a488ebad2
Joffrey <joffrey.berrier@gmail.com>
2018-06-29 15:21:04 +0200

    change config rails

--- cypress/integration/views/pages/home_spec.js
+++ cypress/integration/views/pages/home_spec.js
@@ -1,36 +1,41 @@
-/// <reference types="Cypress" />
+// <reference types="Cypress" />
 
-context('Window', () => {
-  beforeEach(() => {
-    //connect on website
-    cy.visit('http://127.0.0.1:3000/')
-    cy.visit('localhost:3000')
-    cy.visit('/');
+// context('Window', () => {
+  describe('Testing', () => {
+  // beforeEach(() => {
+  //   //connect on website
+  //   cy.visit('http://127.0.0.1:3000/')
 
+  //   //connection
+  //   cy.get('[href="/users/sign_in"]').click();
+  //   cy.get('#user_email').then(($input) => {
+  //     $input.val('user1@gmail.com')
+  //   });
+  //   cy.get('#user_password').then(($input) => {
+  //     $input.val('testtest')
+  //   });
+  //   cy.get('.btn').click();
+  // })
 
-    cy.request('http://127.0.0.1:3000/')
-    cy.request('localhost:3000')
-    cy.request('/');
-    
-    //connection
-    cy.get('[href="/users/sign_in"]').click();
-    cy.get('#user_email').then(($input) => {
-      $input.val('user1@gmail.com')
-    });
-    cy.get('#user_password').then(($input) => {
-      $input.val('testtest')
-    });
-    cy.get('.btn').click();
+  // describe('Testing', () => {
+  //   it('can i have Hello Worl', () => {
+  //     cy.get('#app').contains('Hello World')
+  //   });
+  //   it('All products', () => {
+  //     cy.get('.btn').click()
+  //     cy.get('h1').contains('Mes produits')
+  //   });
+  // });
+
+  it('can i access to 127.0.0.1', () => {
+    cy.request('http://127.0.0.1:3000')
   })
 
-  describe('Testing', () => {
-    it('can i have Hello Worl', () => {
-      cy.get('#app').contains('Hello World')
-    });
-    it('All products', () => {
-      cy.get('.btn').click()
-      cy.get('h1').contains('Mes produits')
-    });
-  });
-  
+  it('can i access to localhost', () => {
+    cy.request('localhost:3000')
+  })
+
+  it('can i access to /', () => {
+    cy.request('/');
+  })
 });
