8411f286917e06c0930e49a80fb016bccfcb3283
Joffrey <joffrey.berrier@gmail.com>
2018-07-01 22:46:19 +0200

    test travis

--- cypress/integration/views/pages/home_spec.js
+++ cypress/integration/views/pages/home_spec.js
@@ -29,13 +29,9 @@
 
   it('can i access to 127.0.0.1', () => {
     cy.request('http://127.0.0.1:3000')
-  })
+  });
 
   it('can i access to localhost', () => {
     cy.request('localhost:3000')
-  })
-
-  it('can i access to /', () => {
-    cy.request('/');
-  })
+  });
 });
