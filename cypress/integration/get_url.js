d955303ff546f23459d5ef04739348ec78e1d80c
Joffrey <joffrey.berrier@gmail.com>
2018-07-18 11:05:21 +0200

    add 127.0.0.1

--- cypress/integration/get_url.js
+++ cypress/integration/get_url.js
@@ -3,7 +3,7 @@
 // context('Window', () => {
 describe('Testing', () => {
   it('can i access to 127.0.0.1', () => {
-    cy.request('/');
+    cy.request('127.0.0.1:3000');
   });
 
   it('can i access to localhost', () => {
